35399d212e2db7308ecb7bce62a6d5e8f2a00e6d
loovjo <jonathan.loov@gmail.com>
2019-03-25 20:34:13 +0100

    More type inference

--- src/HighRep.hs
+++ src/HighRep.hs
@@ -36,8 +36,12 @@ data TypeContext =
     }
     deriving (Show, Eq)
 
+emptyTCtx = TypeContext { getGenerics = M.empty, getVariables = M.empty }
+
 data TypeError
-    = TypeMismatchError (HType, AstCtx) HType
+    = TypeMismatchError AstCtx HType HType -- Expected, real
+    | VarNotFoundError AstCtx String
+    deriving (Show)
 
 
 letters = "abcdefghijklmnopqrstuvqxyz"
@@ -51,6 +55,66 @@ varNames =
 
 newVarName :: TypeContext -> String
 newVarName tctx = head $ filter (not . flip M.member (getGenerics tctx)) varNames
+
+unifyAT :: TypeContext -> Ast -> HType -> Either TypeError (TypeContext, HType)
+unifyAT tctx (RepTree ctx br) ty =
+    let simple ty2 =
+            case unifyTT tctx ty ty2 of
+                Just x -> Right x
+                Nothing -> Left $ TypeMismatchError ctx ty2 ty
+    in case br of
+        ABottom -> simple $ TConstructor "Bottom" []
+        AVar name ->
+            case M.lookup name $ getVariables tctx of
+                Just ty' -> simple ty'
+                Nothing -> Left $ VarNotFoundError ctx name
+        ANum _ -> simple $ TConstructor "Int" []
+        ALet [] ex -> unifyAT tctx ex ty
+        ALet (AVBind var expr:rest) ex ->
+            case M.lookup var $ getVariables tctx of
+                Just vty -> do
+                    (tctx', _) <- unifyAT tctx expr vty
+                    unifyAT tctx' (RepTree ctx $ ALet rest ex) ty
+                Nothing -> do
+                    let new = newVarName tctx
+                    (tctx', vty) <- unifyAT tctx expr $ TNamed new
+                    let tctx'' = tctx' { getVariables = M.insert var vty $ getVariables tctx' }
+                    (tctx''', res) <- unifyAT tctx'' (RepTree ctx $ ALet rest ex) ty
+                    let rtctx =
+                            tctx''' {
+                                getVariables = M.delete var $ getVariables tctx''',
+                                getGenerics = M.delete new $ getVariables tctx'''
+                            }
+                    return $ (rtctx, res)
+        ALet (ATBind var vty:rest) ex ->
+            if M.member var $ getVariables tctx
+                then do
+                    (tctx', _) <- unifyAT tctx (RepTree ctx $ AVar var) vty
+                    unifyAT tctx' (RepTree ctx $ ALet rest ex) ty
+                else do
+                    let tctx' = tctx { getVariables = M.insert var vty $ getVariables tctx }
+                    (tctx'', res) <- unifyAT tctx' (RepTree ctx $ ALet rest ex) ty
+                    return $ (tctx'' { getVariables = M.delete var $ getVariables tctx'' }, res)
+        ALambda [] ex -> unifyAT tctx ex ty
+        ALambda (var:rest) ex ->
+            do
+                let new = newVarName tctx
+                let tctx' = tctx { getVariables = M.insert var (TNamed new) $ getVariables tctx }
+                (tctx'', res) <- unifyAT tctx' ex ty
+
+                let varTy =
+                        case M.lookup new $ getGenerics tctx'' of
+                            Just x -> x
+                            Nothing -> TNamed new
+
+                let rtctx =
+                        tctx'' {
+                            getVariables = M.delete var $ getVariables tctx'',
+                            getGenerics = M.delete new $ getVariables tctx''
+                        }
+
+                return $ (rtctx, TFunction varTy res)
+
 
 
 unifyTT :: TypeContext -> HType -> HType -> Maybe (TypeContext, HType)
