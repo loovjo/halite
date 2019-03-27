0a87def282da9ddadebd8980b392525a1bcfcf87
loovjo <jonathan.loov@gmail.com>
2019-03-27 20:17:36 +0100

    More tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 import qualified Data.Map as M
+import qualified Data.Set as S
 import Ast
 import HType
 import HighRep
@@ -15,18 +16,36 @@ unitTest text x y = do
     putStr $ colBlue ++ text ++ "... "
     if x == y
         then putStrLn $ colGreen ++ "Test passed"
-        else putStrLn $ colRed ++ show x ++ " is not " ++ show y
+        else putStrLn $ colRed ++ "\nGot:    " ++ show x ++ "\nWanted: " ++ show y
 
 main :: IO ()
 main = foldl1 (>>)
     [ putStrLn ""
     , testUnifyTT
+    , testFrees
     , testNewVarName
     ]
 
 g2t generics = TypeContext { getGenerics = generics, getVariables = M.empty}
 
-testUnifyTTUtil :: (M.Map String HType, HType, HType) -> (M.Map String HType, HType) -> IO ()
+testFrees :: IO ()
+testFrees = do
+    unitTest "Frees none"
+        (frees $ TConstructor "Map" [TConstructor "Int" [], TConstructor "List" [TConstructor "Char" []]])
+        S.empty
+    unitTest "Frees one"
+        (frees $ TConstructor "Map" [TNamed "a", TConstructor "List" [TConstructor "Char" []]])
+        $ S.singleton "a"
+    unitTest "Frees many"
+        (frees $ TConstructor "Map" [TNamed "a", TConstructor "Either" [TNamed "x", TNamed "y"]])
+        $ S.fromList ["a", "x", "y"]
+
+    unitTest "Add frees"
+        (addFrees emptyTCtx $ S.fromList ["a", "x", "y"])
+        (g2t $ M.fromList [("a", Exists), ("x", Exists), ("y", Exists)])
+
+
+testUnifyTTUtil :: (M.Map String GenState, HType, HType) -> (M.Map String GenState, HType) -> IO ()
 testUnifyTTUtil (generics, ty1, ty2) (generics', ty) =
     let tctx = g2t generics
         tctx' = g2t generics'
@@ -37,25 +56,45 @@ testUnifyTTUtil (generics, ty1, ty2) (generics', ty) =
 testUnifyTT :: IO ()
 testUnifyTT = do
     testUnifyTTUtil
-        (M.empty, TConstructor "List" [TConstructor "Int" []], TNamed "a")
-        (M.fromList [("a", TConstructor "List" [TConstructor "Int" []])],
+        (M.fromList [("a", Exists)], TConstructor "List" [TConstructor "Int" []], TNamed "a")
+        (M.fromList [("a", SameAs $ TConstructor "List" [TConstructor "Int" []])],
             TConstructor "List" [TConstructor "Int" []])
 
     testUnifyTTUtil
-        (M.empty, TConstructor "List" [TNamed "a"], TConstructor "List" [TNamed "b"])
-        (M.fromList [("b", TNamed "a")], TConstructor "List" [TNamed "a"])
+        (M.fromList [("a", Exists), ("b", Exists)],
+            TConstructor "List" [TNamed "a"],
+            TConstructor "List" [TNamed "b"])
+        (M.fromList [("a", SameAs $ TNamed "b"), ("b", Exists)],
+            TConstructor "List" [TNamed "b"])
 
 
     testUnifyTTUtil
-        (M.empty, TConstructor "List" [TNamed "a"], TConstructor "List" [TConstructor "Int" []])
-        (M.fromList [("a", TConstructor "Int" [])], TConstructor "List" [TConstructor "Int" []])
+        (M.fromList [("a", Exists)],
+            TConstructor "List" [TNamed "a"],
+            TConstructor "List" [TConstructor "Int" []])
+        (M.fromList [("a", SameAs $ TConstructor "Int" [])],
+            TConstructor "List" [TConstructor "Int" []])
 
     testUnifyTTUtil
-        (M.empty,
+        (M.fromList [("a", Exists), ("b", Exists)],
             TFunction (TConstructor "Int" []) (TNamed "a"),
             TFunction (TNamed "b") (TConstructor "Int" []))
-        (M.fromList [("a", TConstructor "Int" []), ("b",(TConstructor "Int" []))],
+        (M.fromList [("a", SameAs $ TConstructor "Int" []), ("b", (SameAs $ TConstructor "Int" []))],
             TFunction (TConstructor "Int" []) (TConstructor "Int" []))
+
+    testUnifyTTUtil
+        (M.fromList [("a", Exists)],
+            TFunction (TNamed "a") (TNamed "a"),
+            TFunction (TConstructor "Int" []) (TConstructor "Int" []))
+        (M.fromList [("a", SameAs $ TConstructor "Int" [])],
+            TFunction (TConstructor "Int" []) (TConstructor "Int" []))
+
+    testUnifyTTUtil
+        (M.fromList [("a", Exists), ("b", Exists)],
+            TFunction (TNamed "a") (TFunction (TNamed "b") (TNamed "a")),
+            TFunction (TConstructor "Int" []) (TFunction (TConstructor "Int" []) (TConstructor "Int" [])))
+        (M.fromList [("a", SameAs $ TConstructor "Int" []), ("b", SameAs $ TConstructor "Int" [])],
+            TFunction (TConstructor "Int" []) (TFunction (TConstructor "Int" []) (TConstructor "Int" [])))
 
 testNewVarName :: IO ()
 testNewVarName = do
